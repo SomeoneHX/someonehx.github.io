8045bdb47156a147f068a1dcfc55ef9ed75dce74
SomeoneHX <baohouxun@163.com>
2026-06-30 21:10:35 +0800

    fix: use router.back() for tag filter button

--- src/views/BlogView.vue
+++ src/views/BlogView.vue
@@ -7,10 +7,10 @@
             <VIcon icon="mdi:tag-outline" width="22" class="blog__title-icon" />
             {{ filterLabel }}
           </h1>
-          <router-link to="/tags/" class="blog__clear">
+          <button class="blog__clear" @click="goBack">
             <VIcon icon="mdi:arrow-left" width="14" class="blog__clear-icon" />
-            全部标签
-          </router-link>
+            返回
+          </button>
         </template>
         <template v-else>
           <h1 class="blog__title">
@@ -44,6 +44,14 @@ const router = useRouter()
 
 function goToTag(tag) {
   router.push(`/tags/${tag}/`)
+}
+
+function goBack() {
+  if (window.history.length > 1) {
+    router.back()
+  } else {
+    router.push('/tags/')
+  }
 }
 
 const filterLabel = computed(() => {
